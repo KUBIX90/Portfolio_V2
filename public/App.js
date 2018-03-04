317893a776d49bdc86105b115713c03b224fd4bd
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-04 23:13:36 +0000

    links added to icons & off canvas navbar functionality added

--- public/App.js
+++ public/App.js
@@ -1,3 +1,7 @@
 document.getElementById("menu-icon").addEventListener("click", function(){
 	document.getElementById("navbar").classList.add("navbar--translate");
 })
+
+document.getElementById("menu_close").addEventListener("click", function(){
+	document.getElementById("navbar").classList.remove("navbar--translate");
+})
