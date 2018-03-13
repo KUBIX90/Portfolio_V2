b623f770c64bbe4df6f17999d1ca2ea12d7e4e0a
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-13 23:45:20 +0000

    first modal completed

--- public/App.js
+++ public/App.js
@@ -5,3 +5,15 @@ document.getElementById("menu-icon").addEventListener("click", function(){
 document.getElementById("menu_close").addEventListener("click", function(){
 	document.getElementById("navbar").classList.remove("navbar--translate");
 })
+
+document.querySelector(".container_portfolio-image--overlay").addEventListener("click", function(){
+	document.querySelector(".modal-content").style.display = "block";
+	document.querySelector(".modal").style.opacity = "1";	
+	document.querySelector(".modal").style.visibility = "visible";
+})
+
+document.querySelector(".modal-content_close").addEventListener("click", function(){
+	document.querySelector(".modal-content").style.animation = "removeModal 1s";
+	document.querySelector(".modal").style.opacity = "0";	
+	document.querySelector(".modal").style.visibility = "hidden";
+})
