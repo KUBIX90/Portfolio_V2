9bc9bb0ed1132353f8d36e32267d34c5a2213370
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-20 20:07:46 +0000

    added select variable

--- public/App.js
+++ public/App.js
@@ -1,19 +1,21 @@
-document.querySelector("#menu-icon").addEventListener("click", function(){
-	document.querySelector("#navbar").style.transform = "translateX(0%)";
+var select = document.querySelector.bind(document);
+
+select("#menu-icon").addEventListener("click", function(){
+	select("#navbar").style.transform = "translateX(0%)";
 })
 
-document.querySelector(".close").addEventListener("click", function(){
-	document.querySelector("#navbar").style.transform = "translateX(-100%)";
+select(".close").addEventListener("click", function(){
+	select("#navbar").style.transform = "translateX(-100%)";
 })
 
-document.querySelector(".project_image-overlay").addEventListener("click", function(){
-	document.querySelector(".content").style.animation = "showModal 1s";
-	document.querySelector("#modals").style.opacity = "1";	
-	document.querySelector("#modals").style.visibility = "visible";
+select(".project_image-overlay").addEventListener("click", function(){
+	select(".content").style.animation = "showModal 1s";
+	select("#modals").style.opacity = "1";	
+	select("#modals").style.visibility = "visible";
 })
 
-document.querySelector(".modal_close-content").addEventListener("click", function(){
-	document.querySelector(".content").style.animation = "removeModal 1s";
-	document.querySelector("#modals").style.opacity = "0";	
-	document.querySelector("#modals").style.visibility = "hidden";
+select(".modal_close-content").addEventListener("click", function(){
+	select(".content").style.animation = "removeModal 1s";
+	select("#modals").style.opacity = "0";	
+	select("#modals").style.visibility = "hidden";
 })
