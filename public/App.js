b7b71adacba48cb2b7c6c371165168bc2f68df4b
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-20 19:49:38 +0000

    CSS selectors renamed and organised

--- public/App.js
+++ public/App.js
@@ -6,14 +6,14 @@ document.querySelector(".close").addEventListener("click", function(){
 	document.querySelector("#navbar").style.transform = "translateX(-100%)";
 })
 
-document.querySelector(".container_portfolio-image--overlay").addEventListener("click", function(){
-	document.querySelector(".modal-content").style.animation = "showModal 1s";
-	document.querySelector(".modal").style.opacity = "1";	
-	document.querySelector(".modal").style.visibility = "visible";
+document.querySelector(".project_image-overlay").addEventListener("click", function(){
+	document.querySelector(".content").style.animation = "showModal 1s";
+	document.querySelector("#modals").style.opacity = "1";	
+	document.querySelector("#modals").style.visibility = "visible";
 })
 
-document.querySelector(".modal-content_close").addEventListener("click", function(){
-	document.querySelector(".modal-content").style.animation = "removeModal 1s";
-	document.querySelector(".modal").style.opacity = "0";	
-	document.querySelector(".modal").style.visibility = "hidden";
+document.querySelector(".modal_close-content").addEventListener("click", function(){
+	document.querySelector(".content").style.animation = "removeModal 1s";
+	document.querySelector("#modals").style.opacity = "0";	
+	document.querySelector("#modals").style.visibility = "hidden";
 })
