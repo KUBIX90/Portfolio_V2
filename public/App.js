8d9e123b4fc39dfb33bad168701df10196e708cc
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-21 23:02:03 +0000

    Implementing multiple modals using CSS

--- public/App.js
+++ public/App.js
@@ -8,7 +8,7 @@ select(".close").addEventListener("click", function(){
 	select("#navbar").style.transform = "translateX(-100%)";
 })
 
-select(".project_image-overlay").addEventListener("click", function(){
+/*select(".project_image-overlay").addEventListener("click", function(){
 	select(".content").style.animation = "showModal 1s";
 	select("#modals").style.opacity = "1";	
 	select("#modals").style.visibility = "visible";
@@ -18,4 +18,4 @@ select(".modal_close-content").addEventListener("click", function(){
 	select(".content").style.animation = "removeModal 1s";
 	select("#modals").style.opacity = "0";	
 	select("#modals").style.visibility = "hidden";
-})
+})*/
