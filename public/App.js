a57526cfda15ed8e6809395c29e1f6a9ae380423
KUBIX90 <mark.j.kubik@gmail.com>
2018-03-27 21:48:43 +0100

    modal responsiveness added & html issues errors amended

--- public/App.js
+++ public/App.js
@@ -7,15 +7,3 @@ select("#menu-icon").addEventListener("click", function(){
 select(".close").addEventListener("click", function(){
 	select("#navbar").style.transform = "translateX(-100%)";
 })
-
-/*select(".project_image-overlay").addEventListener("click", function(){
-	select(".content").style.animation = "showModal 1s";
-	select("#modals").style.opacity = "1";	
-	select("#modals").style.visibility = "visible";
-})
-
-select(".modal_close-content").addEventListener("click", function(){
-	select(".content").style.animation = "removeModal 1s";
-	select("#modals").style.opacity = "0";	
-	select("#modals").style.visibility = "hidden";
-})*/
