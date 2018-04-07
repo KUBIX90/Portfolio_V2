edd4d6798bbdeaf6400844bc167c7f3bb9484f4a
KUBIX90 <mark.j.kubik@gmail.com>
2018-04-07 20:02:03 +0100

    amended spacing within navbar

--- public/App.js
+++ public/App.js
@@ -2,6 +2,7 @@ var select = document.querySelector.bind(document);
 
 select("#menu-icon").addEventListener("click", function(){
 	select("#navbar").style.transform = "translateX(0%)";
+	select("#navbar").style.opacity = 1;
 })
 
 select(".close").addEventListener("click", function(){
