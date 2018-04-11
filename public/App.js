b28e21ab14dba8e7b29db1558396a266b46e4f9a
KUBIX90 <mark.j.kubik@gmail.com>
2018-04-11 23:15:41 +0100

    Amended modal animation

--- public/App.js
+++ public/App.js
@@ -2,11 +2,11 @@ var select = document.querySelector.bind(document);
 
 select("#menu-icon").addEventListener("click", function(){
 	select("#navbar").style.transform = "translateX(0%)";
-	select("#navbar").style.opacity = 1;
+	select("#navbar").style.opacity = 1; 
 	select(".text-box").style.left = "10%";
-})
+});
 
 select(".close").addEventListener("click", function(){
 	select("#navbar").style.transform = "translateX(-100%)";
 	select(".text-box").style.left = "0%";
-})
+});
